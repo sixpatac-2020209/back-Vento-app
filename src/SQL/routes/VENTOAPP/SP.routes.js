44fa168eaf28c2eb63cf73898d00e6edd8040e53
Sduard Sipaque <sixpatac-2020209@kinal.edu.gt>
2022-10-28 16:09:23 -0600

    Cambios

--- src/SQL/routes/VENTOAPP/SP.routes.js
+++ src/SQL/routes/VENTOAPP/SP.routes.js
@@ -18,9 +18,9 @@ api.put('/SP8/:id', spController.SP_RESULTADOS8);
 api.put('/SP9/:id', spController.SP_RESULTADOS9);
 api.put('/SP10/:id', spController.SP_RESULTADOS10);
 
-api.put('/SP11', spController.SP_RESULTADOS11);
-api.put('/SP12', spController.SP_RESULTADOS12);
-api.put('/SP13', spController.SP_RESULTADOS13);
-api.put('/SP14', spController.SP_RESULTADOS14);
+api.put('/SP11/:id', spController.SP_RESULTADOS11);
+api.put('/SP12/:id', spController.SP_RESULTADOS12);
+api.put('/SP13/:id', spController.SP_RESULTADOS13);
+api.put('/SP14/:id', spController.SP_RESULTADOS14);
 
 module.exports = api
